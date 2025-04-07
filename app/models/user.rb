1b918dad2524c5e92432bd496143dd40497740f4
Dicarvalho <dicarvalho.ti@gmail.com>
2025-04-07 10:02:03 -0300

    Add Code for Deliver Email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,16 @@ class User < ApplicationRecord
   # Adicionado o '%'(Percent Sign) para permitir buscas parciais usando uma interpolação de string
   # Exemplo: User.by_username('ma') irá buscar por todos os usuários cujo username contém 'ma'
   scope :by_username, -> (username) { where('username LIKE ?', "%#{username}%") if username.present? }
+
+  after_create :send_welcome_email
+
+
+
+
+  private 
+
+  def send_welcome_email
+    UserMailer.welcome_to_app(self).deliver_now
+  end
   
 end
